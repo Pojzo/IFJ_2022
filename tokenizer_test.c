24a3e98c59aea22d232f5e871e84c7bfd12aad26
Pojzo <pojzinko8@gmail.com>
2022-11-07 19:08:37 +0100

    :U+1F913:

--- tokenizer_test.c
+++ tokenizer_test.c
@@ -3,17 +3,25 @@
 #include "tokenizer.h"
 
 int main() {
-    char *buffer = "gazdik";
+    char *buffer = "                    $gazdik;;;  $kokot    ;";
     token_storage_t *token_storage = token_storage_create();
+    /*
     token_storage_add(token_storage, TOK_ID, buffer, 2);
     token_storage_add(token_storage, TOK_KEYWORD, buffer, 3);
     token_storage_add(token_storage, TOK_SEPARATOR, buffer, 4);
     token_storage_add(token_storage, TOK_LITERAL, buffer, 5);
+    printf("Tokenizer->num_tokens: %d\nTokenizer->array_len %d\n", token_storage->num_tokens, token_storage->array_len);
     for(int i = 0; i < 4; i++) {
         token_print(token_storage->tokens[i]);
     }
-    printf("Tokenizer->num_tokens: %d\nTokenizer->array_len %d\n", token_storage->num_tokens, token_storage->array_len);
+
+    */
+    int error = dka(buffer, strlen(buffer), token_storage);
+    for(int i = 0; i < token_storage->array_len; i++) {
+        token_print(token_storage->tokens[i]);
+    }
+    printf("Length of token storage -> %d\n", token_storage->num_tokens);
+
     token_storage_free(token_storage);
-    // dka(buffer, buffer_len, tokens);
-    return 0;
+    return error;
 }
